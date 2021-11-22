7a48686a1abe7f9a016dc895b975b9951649511a
deji-ice <ayodejiatanda1@gmail.com>
2021-11-23 00:51:53 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Profile from './profile/Profile';
+import logo from './IMG-20191029-WA0007.jpg';
+
+
+console.log(logo)
 
 function App() {
+
+  function handleName(){
+    alert(this.fullName)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="App" style={{display:"flex", justifyContent:"center"}}>
+  
+     <Profile fullName='Ayodeji Atanda' 
+     handle = {handleName}
+     bio= "i love watching movies, coding and musiccc plus twitter lol " 
+     profession='Software Developer'>
+        <img style={{width:'8rem' }} alt='timer' src={logo}/>
+         </Profile>
     </div>
   );
 }
+
+
 
 export default App;
